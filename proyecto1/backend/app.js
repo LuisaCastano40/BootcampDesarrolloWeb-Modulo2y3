371377542f8e3c3de8466d1d6176f63e69b99e9d
Luisa Castaño <luisa.castano40@gmail.com>
2024-06-21 20:04:54 -0500

    [actualización] modelos y controladores

--- proyecto1/backend/app.js
+++ proyecto1/backend/app.js
@@ -1,17 +1,23 @@
-//1. importamos las dependencias y modulos que necesitamos
+//1. importamos las dependencias y módulos que necesitamos
 import express from 'express';
 import dotenv from 'dotenv';
 //Variables de entorno -> nos guardan informacion delicada
-
+import connectionMongo from './config/db.js';
 
 
 //2.Configurar el uso de nuestro servidor y de nuestras variables de
-
 const app = express(); //configuramos nuestro servidor
 dotenv.config(); //configuramos nuestras variables de entorno
-
 const port = process.env.PORT;
 
+//conéctese a la base de datos
+connectionMongo();
+
+
+//Prueba
+// app.get('/',(req, res)=>{
+//     res.send('HOLAAAAA, ESTO ES UNA PETICIÓN A MI SERVIDOR');
+// });
 
 
 //3. escuchar nuestro servidor (ejecutarlo)
