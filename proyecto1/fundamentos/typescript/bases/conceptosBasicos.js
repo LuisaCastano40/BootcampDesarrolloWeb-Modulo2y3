dda121b67d74556cd93713f33de0f62662353ba6
Luisa Castaño <luisa.castano40@gmail.com>
2024-06-26 21:42:33 -0500

    [actualizacion] funciones

--- proyecto1/fundamentos/typescript/bases/conceptosBasicos.js
+++ proyecto1/fundamentos/typescript/bases/conceptosBasicos.js
@@ -5,46 +5,87 @@ console.log('hola soy typescript');
 //Si importa el tipo de variable
 //asume que esa variable SOLO recibe el tipo de datos que usted guardó por primera vez
 //declaración implícita
-var miVariable = "abc";
+let miVariable = "abc";
 miVariable = 'luisa';
-var miNumero = 20;
+let miNumero = 20;
 miNumero = 30;
 //Declaración explícita
-var miNumero2;
+let miNumero2;
 //Asignas un valor respetando el tipo declarado
 miNumero2 = 30;
 //console.log(miNumero2);
 //TIPOS DE DATOS 
 //DATOS PRIMITIVOS -> Básicos (string, numero, booleanos, undefined, null)
 //numérico (-1, 10, 5.6 ...)
-var numero;
+let numero;
 //String -> texto
-var texto;
+let texto;
 //Booleanos
-var buleano;
+let buleano;
 //Indefinidos
-var indefinido;
+let indefinido;
 //Nulos
-var nulo;
-numero = 2;
+let nulo;
+numero = 20;
 texto = "hola";
 buleano = false;
 indefinido = undefined;
 nulo = null;
 //TIPOS DE DATOS ESPECIALES
-//Arrays , objetos, unkwon -> desconocido y any
+//Arrays , objetos, unkwon -> desconocido y any -> cualquiera
 //implicitamente es un arreglo de números
-var arreglo = [1, 2, 5];
+let arreglo = [1, 2, 5];
 //declaración explícita de un arreglo
 //forma 1
-var arreglo2;
-var arreglo3;
-var arreglo4;
+let arreglo2;
+let arreglo3;
+let arreglo4;
 arreglo = [10, 70, 35];
 arreglo2 = [100, 700, 350];
 arreglo3 = ['luisa', 'cristian', 'laura'];
 arreglo4 = [true, false];
 //forma 2 de declarar tipos de datos en los arreglos
-var miArreglo;
+let miArreglo;
 miArreglo = ['luisa', 'angelo'];
-//objetos - known - any - funciones
+//objetos - unknown - any - funciones
+//nuestras variables reciben datos de cualquier tipo
+//no es tan usado 
+let desconocido; //usted recibe cualquier tipo de dato, porque no sabe qué va a llegar
+let cualquiera; //no me importa lo que llegue, guárdemelo 
+desconocido = 'luisa';
+desconocido = 3;
+desconocido = [true, false];
+cualquiera = false;
+cualquiera = undefined;
+cualquiera = null;
+// OBJETOS 
+let miObjeto; // -> [] o {}
+//declaración explicita de un objeto literal
+// Asignación -> = -> valores y Declaración -> : -> tipo
+let miObjetoCarro;
+miObjetoCarro = {
+    id: 1,
+    marca: 'BMW',
+    vendido: true
+};
+//FUNCIONES
+//Las funciones pueden devolver valores o puede que no devuelvan nada
+let retorno;
+function miFuncion1() {
+    console.log("soy función 1");
+}
+//ejecute
+miFuncion1();
+//guárdese en una variable
+//retorno = miFuncion1();
+//mi variable le puedo dar parámetros -> le asignamos tipos
+// fn flecha -> puede que retorne o no retorne
+const miFuncion2 = (num1, num2) => {
+    return num1 * num2;
+};
+//Además yo puedo especificar el tipo de la respuesta que me va a dar
+function miFuncion3(num1, num2) {
+    return num1 + num2;
+}
+retorno = miFuncion3(2, 5);
+console.log(retorno);
--- proyecto1/fundamentos/typescript/bases/conceptosBasicos.ts
+++ proyecto1/fundamentos/typescript/bases/conceptosBasicos.ts
@@ -36,7 +36,7 @@ let indefinido : undefined;
 //Nulos
 let nulo : null;
 
-numero = 2;
+numero = 20;
 texto = "hola";
 buleano = false;
 indefinido = undefined;
@@ -44,7 +44,7 @@ nulo = null;
 
 
 //TIPOS DE DATOS ESPECIALES
-//Arrays , objetos, unkwon -> desconocido y any
+//Arrays , objetos, unkwon -> desconocido y any -> cualquiera
 //implicitamente es un arreglo de números
 let arreglo = [1, 2, 5];
 
@@ -63,4 +63,66 @@ arreglo4 = [true, false];
 let miArreglo : string[];
 miArreglo = ['luisa', 'angelo'];
 
-//objetos - known - any - funciones
+//objetos - unknown - any - funciones
+//nuestras variables reciben datos de cualquier tipo
+//no es tan usado 
+let desconocido : unknown; //usted recibe cualquier tipo de dato, porque no sabe qué va a llegar
+let cualquiera : any;//no me importa lo que llegue, guárdemelo 
+
+desconocido = 'luisa';
+desconocido = 3;
+desconocido = [true, false];
+cualquiera = false;
+cualquiera = undefined;
+cualquiera = null;
+
+
+// OBJETOS 
+let miObjeto : object; // -> [] o {}
+
+//declaración explicita de un objeto literal
+// Asignación -> = -> valores y Declaración -> : -> tipo
+
+let miObjetoCarro : {
+    id: number;
+    marca: string;
+    color?: string; //ref no sabe si ese elemnto va o no a darse
+    vendido:boolean;
+};
+
+miObjetoCarro = {
+    id: 1,
+    marca: 'BMW',
+    vendido: true
+};
+
+//FUNCIONES
+//Las funciones pueden devolver valores o puede que no devuelvan nada
+// devolver datos de tipo number, string, boolean, object
+//las que no devuelven -> :void -> sólo me ejecuto
+
+let retorno : any;
+
+function miFuncion1() : void {
+    console.log("soy función 1");
+}
+//ejecute
+miFuncion1();
+//guárdese en una variable
+//retorno = miFuncion1(); -> nos retorna indefinido
+
+
+//mi variable le puedo dar parámetros -> le asignamos tipos
+// fn flecha -> puede que retorne o no retorne
+const miFuncion2 = (num1 : number , num2 : number) =>  {
+    return num1 * num2;
+}
+    //retorno = miFuncion2() -> number
+
+//Además yo puedo especificar el tipo de la respuesta que me va a dar
+function miFuncion3 (num1 : number, num2 : number) : number{
+    return num1 + num2;
+}
+
+retorno = miFuncion3(2,5); //-> number
+    console.log(retorno);
