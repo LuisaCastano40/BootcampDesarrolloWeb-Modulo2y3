294772e9591653f7d969b05074082f827e163667
Luisa Castaño <luisa.castano40@gmail.com>
2024-07-03 17:37:30 -0500

    [actualizacion] frontend 3/07/2024

--- proyecto1/frontend/src/app/components/navegacion/navegacion.component.ts
+++ proyecto1/frontend/src/app/components/navegacion/navegacion.component.ts
@@ -2,15 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  //ESTO SE USA EN HTML -> Para poder usar el comoponente
   selector: 'app-navegacion',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './navegacion.component.html',
   styleUrl: './navegacion.component.css'
 })
-
-//ESTA CLASE ES LA QUE DEBO IMPORTAR
 export class NavegacionComponent {
 
 }
