8029249f4fdb7506b928b3689685a4c1e7bad67f
Luisa castaño <luisa.castano40@eia.edu.co>
2024-07-23 17:07:47 -0500

    [update] actualizacion proyecto 2

---
+++ proyecto2/backend/src/models/user.model.js
@@ -0,0 +1,25 @@
+// 1. Importacion de dependencias y módulos
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    nombreCompleto:{
+        type: String,
+        required: true
+    },
+    correo:{
+        type:String,
+        required: true,
+        unique: true
+    },
+    contrasena:{
+        type:String,
+        required: true
+    },
+    imagen: {
+        type: String
+    }
+
+});
+
+
+export const userModel = mongoose.model('usuario', userSchema);
