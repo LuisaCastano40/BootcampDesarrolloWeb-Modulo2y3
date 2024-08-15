ed963c81334cab5510ce3d6f1e1f8272155c8e1e
Luisa castaño <luisa.castano40@eia.edu.co>
2024-08-15 16:21:47 -0500

    [update] services

--- proyecto2/frontend/src/app/services/admin.service.ts
+++ proyecto2/frontend/src/app/services/admin.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  // inyección de dependencias
+  private httpClient = inject(HttpClient);
+  private URL_ADMIN = 'http://localhost:3000/admin';
+
+  // la petición get -> acción de hacer la petición
+  getAdmins(){
+    return this.httpClient.get(this.URL_ADMIN);
+  }
+
 }
